a13087db042205fa1706b4d4efc0f7ab4c530f24
paprika <feira@naver.com>
2020-07-21 00:59:07 +0900

    Image Color Conversion

---
+++ Image Color Conversion/swLab19f/resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// swLab19f.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_swLab19fTYPE                130
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_2                            32777
+#define ID_3                            32778
+#define ID_4                            32779
+#define ID_5                            32780
+#define ID_FOPEN01                      32781
+#define ID_FOPEN02                      32782
+#define ID_PopUpIM01                    32783
+#define ID_PopUpIM02                    32784
+#define ID_DarkenI                      32785
+#define ID_DarkenImg                    32786
+#define ID_PopUpIMR                     32787
+#define ID_SAVE_IMG                     32788
+#define ID_BrightenImg                  32789
+#define ID_24Col                        32790
+#define ID_24ColToGray                  32791
+#define ID_16ColToGray                  32792
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32793
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
